96c02910fb72a5907549bbb2d0d791ab0665bbdb
Ishwar Choudhary <ishwar@github.com>
2020-07-13 16:23:58 +0530

    added multiple carousels

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ishwarcarousel/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ishwarcarousel from './components/ishwarCarousel.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ishwarcarousel
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Helvetica,;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
---
+++ src/components/ishwarCarousel.vue
@@ -0,0 +1,167 @@
+<template>
+  <div class="d-flex justify-content-center">
+    <div class="d-flex" style="width:1000px;">
+      <flickity
+        class="flickity"
+        style="width:300px;"
+        :options="flickityOptions"
+      >
+        <div class="carousel-cell">
+          <img src="../assets/I.png" id="images" alt="" />
+        </div>
+        <div class="carousel-cell">
+          <img src="../assets/S.png" id="images" alt="" />
+        </div>
+        <div class="carousel-cell">
+          <img src="../assets/H.png" id="images" alt="" />
+        </div>
+        <div class="carousel-cell">
+          <img src="../assets/W.png" id="images" alt="" />
+        </div>
+        <div class="carousel-cell">
+          <img src="../assets/A.png" id="images" alt="" />
+        </div>
+        <div class="carousel-cell">
+          <img src="../assets/R.png" id="images" alt="" />
+        </div>
+      </flickity>
+      <flickity
+        class="flickity"
+        style="width:300px;"
+        :options="flickityOptions"
+      >
+        <div class="carousel-cell">
+          <img src="../assets/S.png" id="images" alt="" />
+        </div>
+        <div class="carousel-cell">
+          <img src="../assets/H.png" id="images" alt="" />
+        </div>
+        <div class="carousel-cell">
+          <img src="../assets/W.png" id="images" alt="" />
+        </div>
+        <div class="carousel-cell">
+          <img src="../assets/A.png" id="images" alt="" />
+        </div>
+        <div class="carousel-cell">
+          <img src="../assets/R.png" id="images" alt="" />
+        </div>
+        <div class="carousel-cell">
+          <img src="../assets/I.png" id="images" alt="" />
+        </div>
+      </flickity>
+      <flickity
+        class="flickity"
+        style="width:300px;"
+        :options="flickityOptions"
+      >
+        <div class="carousel-cell">
+          <img src="../assets/H.png" id="images" alt="" />
+        </div>
+        <div class="carousel-cell">
+          <img src="../assets/W.png" id="images" alt="" />
+        </div>
+        <div class="carousel-cell">
+          <img src="../assets/A.png" id="images" alt="" />
+        </div>
+        <div class="carousel-cell">
+          <img src="../assets/R.png" id="images" alt="" />
+        </div>
+        <div class="carousel-cell">
+          <img src="../assets/I.png" id="images" alt="" />
+        </div>
+        <div class="carousel-cell">
+          <img src="../assets/S.png" id="images" alt="" />
+        </div>
+      </flickity>
+      <flickity
+        class="flickity"
+        style="width:300px;"
+        :options="flickityOptions"
+      >
+        <div class="carousel-cell">
+          <img src="../assets/W.png" id="images" alt="" />
+        </div>
+        <div class="carousel-cell">
+          <img src="../assets/A.png" id="images" alt="" />
+        </div>
+        <div class="carousel-cell">
+          <img src="../assets/R.png" id="images" alt="" />
+        </div>
+        <div class="carousel-cell">
+          <img src="../assets/I.png" id="images" alt="" />
+        </div>
+        <div class="carousel-cell">
+          <img src="../assets/S.png" id="images" alt="" />
+        </div>
+        <div class="carousel-cell">
+          <img src="../assets/H.png" id="images" alt="" />
+        </div>
+      </flickity>
+      <flickity
+        class="flickity"
+        style="width:300px;"
+        :options="flickityOptions"
+      >
+        <div class="carousel-cell">
+          <img src="../assets/A.png" id="images" alt="" />
+        </div>
+        <div class="carousel-cell">
+          <img src="../assets/R.png" id="images" alt="" />
+        </div>
+        <div class="carousel-cell">
+          <img src="../assets/I.png" id="images" alt="" />
+        </div>
+        <div class="carousel-cell">
+          <img src="../assets/S.png" id="images" alt="" />
+        </div>
+        <div class="carousel-cell">
+          <img src="../assets/H.png" id="images" alt="" />
+        </div>
+        <div class="carousel-cell">
+          <img src="../assets/W.png" id="images" alt="" />
+        </div>
+      </flickity>
+    </div>
+  </div>
+</template>
+
+<script>
+import Flickity from "vue-flickity";
+
+export default {
+  components: {
+    Flickity,
+  },
+  data() {
+    return {
+      items: [1, 1, 1, 1, 1, 1],
+      flickityOptions: {
+        initialIndex: 0,
+        prevNextButtons: false,
+        pageDots: false,
+        groupCells: false,
+        contain: true,
+        autoPlay: 500,
+        freeScroll: true,
+        wrapAround: true,
+        pauseAutoPlayOnHover: false,
+        // draggable: false
+        // any options from Flickity can be used
+      },
+    };
+  },
+};
+</script>
+
+<style scoped>
+.carousel-cell {
+  width: 320px;
+  height: 400px;
+  border-radius: 5px;
+  margin-right: 10px;
+}
+
+#images {
+  width: 40%;
+}
+</style>
